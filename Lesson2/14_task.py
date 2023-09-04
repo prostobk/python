376764dcd0481f6bcd1fa56f7d527729108347c1
Nikolai Baikalov <prostobk18@gmail.com>
2023-09-04 18:06:09 +0300

    Решены задачи 4-го семинара

--- Lesson2/14_task.py
+++ Lesson2/14_task.py
@@ -1,9 +1,23 @@
 #Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
 
-n = int(input('Введите число N: '))
-k = 0
-res = 1
-while res < n+1:
-    print(res, end=' ')
-    k += 1
-    res = 2 ** k
+#n = int(input('Введите число N: '))
+#k = 0
+#res = 1
+#while res < n+1:
+#    print(res, end=' ')
+#    k += 1
+#    res = 2 ** k
+
+list_1 = [1, 2, 3, 4, 5]
+k = 4
+
+count = 0
+count1 = 0
+for i in range(len(list_1)):
+    if k == list_1[i]:
+        count = i
+        count1 = count
+    else:
+        (k-list_1[i]) < k-count and k-list_1[i] > 0
+        count1 = i
+print(list_1[count])
